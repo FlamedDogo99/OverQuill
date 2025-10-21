5214462b57995d409dcb753dd05e5f68067055e3
FlamedDogo99 <96555444+FlamedDogo99@users.noreply.github.com>
2025-10-20 21:33:18 -0600

    Fixed command suggestion hidden overflow

--- tools/package-extension.sh
+++ tools/package-extension.sh
@@ -1,6 +1,6 @@
 node tools/build-extension.js "$1"
 cd build/temp || exit
-zip -r "OverQuill-$1.zip" . -x '.*' -x '**/__MACOSX'
+zip -r "OverQuill-$1.zip" . -x '.*' -x '**/__MACOSX' -x '**/.*'
 mv "OverQuill-$1.zip" "../"
 cd ../ || exit
 if [ -d "temp" ]; then rm -Rf "temp"; fi
